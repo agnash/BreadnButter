300bacb5140d1d19ad79a28bc66b9e370c1c2794
agnash <agnash@gmail.com>
2013-09-22 23:54:34 -0700

    Update QuadRotorUAV_CAD.js

--- QuadRotorUAV_CAD.js
+++ QuadRotorUAV_CAD.js
@@ -1,4 +1,4 @@
-/* copy and paste this code into editor at www.openjscad.org */
+/* This is a work in progress. Copy and paste this code into editor at www.openjscad.org */
 
 function main() {
     return union(
@@ -14,7 +14,123 @@ function main() {
             cube({size: [10.625, 0.75, 0.75]}),
             cube({size: [10.625, 0.625, 0.625]}).translate([0, 0.0625, 0.0625])
         ).translate([-10.625, 10.625, 0]),
-        color('green', cube({size: [5, 5, 0.1]}).rotateZ(45).translate([0.375, (11 - sqrt(12.5)), -0.1])),
-        color('tan', cube({size: [5, 5, 0.1]}).rotateZ(45).translate([0.375, (11 - sqrt(12.5)), 0.75]))
+        color('silver', cube({size: [5, 5, 0.064]}).rotateZ(45).translate([0.375, (11 - sqrt(12.5)), -0.064])),
+        color('silver', cube({size: [5, 5, 0.064]}).rotateZ(45).translate([0.375, (11 - sqrt(12.5)), 0.75])),
+        // start definition of motor bay 1
+        union(
+            // base plate
+            color('silver', cube({size: [1.75, 3, 0.064]}).translate([-0.5, -1.75, -0.064])),
+            // right vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([0.75, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([0.75 + 0.064, 0, 0.064])
+            ),
+            // right vertical support - span
+            rotate([19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))).translate([0.75 + 0.064, 0, 0]),
+            // right vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([1.25, -1.25, 0]), 
+            // right vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([0.75, -1.75, 0])),
+            // left vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([-0.5, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([-0.5, 0, 0.064])
+            ),
+            // left vertical support - span
+            rotate([-19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))),
+            // left vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([-0.5 + 0.064, -1.25, 0]),
+            // left vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([-0.5, -1.75, 0])),
+            // top plate
+            color('silver', cube({size: [1.75, 1.25, 0.064]}).translate([-0.5, 0, 0.75]))
+        ).translate([0, 0, 0]),
+        // motor bay 2
+        union(
+            // base plate
+            color('silver', cube({size: [1.75, 3, 0.064]}).translate([-0.5, -1.75, -0.064])),
+            // right vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([0.75, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([0.75 + 0.064, 0, 0.064])
+            ),
+            // right vertical support - span
+            rotate([19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))).translate([0.75 + 0.064, 0, 0]),
+            // right vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([1.25, -1.25, 0]), 
+            // right vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([0.75, -1.75, 0])),
+            // left vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([-0.5, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([-0.5, 0, 0.064])
+            ),
+            // left vertical support - span
+            rotate([-19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))),
+            // left vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([-0.5 + 0.064, -1.25, 0]),
+            // left vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([-0.5, -1.75, 0])),
+            // top plate
+            color('silver', cube({size: [1.75, 1.25, 0.064]}).translate([-0.5, 0, 0.75]))
+        ).rotateZ(90).translate([11.375, 10.625, 0]),
+        // motor bay 3
+        union(
+            // base plate
+            color('silver', cube({size: [1.75, 3, 0.064]}).translate([-0.5, -1.75, -0.064])),
+            // right vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([0.75, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([0.75 + 0.064, 0, 0.064])
+            ),
+            // right vertical support - span
+            rotate([19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))).translate([0.75 + 0.064, 0, 0]),
+            // right vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([1.25, -1.25, 0]), 
+            // right vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([0.75, -1.75, 0])),
+            // left vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([-0.5, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([-0.5, 0, 0.064])
+            ),
+            // left vertical support - span
+            rotate([-19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))),
+            // left vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([-0.5 + 0.064, -1.25, 0]),
+            // left vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([-0.5, -1.75, 0])),
+            // top plate
+            color('silver', cube({size: [1.75, 1.25, 0.064]}).translate([-0.5, 0, 0.75]))
+        ).rotateZ(180).translate([0.75, 22, 0]),
+        // motor bay 4
+        union(
+            // base plate
+            color('silver', cube({size: [1.75, 3, 0.064]}).translate([-0.5, -1.75, -0.064])),
+            // right vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([0.75, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([0.75 + 0.064, 0, 0.064])
+            ),
+            // right vertical support - span
+            rotate([19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))).translate([0.75 + 0.064, 0, 0]),
+            // right vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([1.25, -1.25, 0]), 
+            // right vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([0.75, -1.75, 0])),
+            // left vertical support - boom
+            difference(
+                color('tan', cube({size: [0.5, 1.25, 0.75]}).translate([-0.5, 0, 0])),
+                cube({size: [(0.5 - 0.064), 1.25, (0.75 - (2 * 0.064))]}).translate([-0.5, 0, 0.064])
+            ),
+            // left vertical support - span
+            rotate([-19.23, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.75, 0], [0.17 + (2 * 0.064), -1.32], [0.064, -1.32]])))),
+            // left vertical support - baseplate, vertical
+            rotate([0, -90, 0], color('tan', linear_extrude({height: 0.064}, polygon([[0.064, 0], [0.17 + (2 * 0.064), 0], [2 * 0.064, -0.5], [0.064, -0.5]])))).translate([-0.5 + 0.064, -1.25, 0]),
+            // left vertical support - baseplate, horizontal
+            color('tan', cube({size: [0.5, 0.5, 0.064]}).translate([-0.5, -1.75, 0])),
+            // top plate
+            color('silver', cube({size: [1.75, 1.25, 0.064]}).translate([-0.5, 0, 0.75]))
+        ).rotateZ(-90).translate([-10.625, 11.375, 0])
     );
 }
