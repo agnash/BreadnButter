c95a7879bbcf278b295e88322b1de3932eaa60b9
agnash <agnash@gmail.com>
2013-12-11 00:39:33 -0800

    Update QuadRotorUAV_CAD_Dynamic.js

--- QuadRotorUAV_CAD_Dynamic.js
+++ QuadRotorUAV_CAD_Dynamic.js
@@ -4,7 +4,7 @@ function getParameterDefinitions() {
   return [
     { name: 'mDiameter', type: 'float', initial: 35.0, caption: "Motor diameter (mm):"},
     { name: 'mMountWidth', type: 'float', initial: 58.0, caption: "Motor mount diameter (mm):"},
-    { name: 'mLength', type: 'float', initial: 18, caption: "Motor-to-motor distance:"},
+    { name: 'mLength', type: 'float', initial: 20, caption: "Motor-to-motor distance:"},
     { name: 'battLength', type: 'float', initial: 5.0, caption: "Length of battery:" },
     { name: 'battWidth', type: 'float', initial: 2.0, caption: "Width of battery:" },
     { name: 'battHeight', type: 'float', initial: 1.0, caption: "Height of battery:" },
@@ -22,37 +22,61 @@ function main(params) {
     return quad;
 }
 
+// lower frame plate object constructor
+function lowerPlate(length, width, thickness) {
+    this.length = length;
+    this.width = width;
+    this.thickness = thickness;
+    this.plate = difference(
+        cube({size: [this.length, this.width, this.thickness]})
+    );
+}
+
+// boom arm object constructor
+function boomArm(length) {
+    this.length = length;
+    this.width = 0.75;
+    this.arm = difference(
+        cube({size: [this.width, this.length, this.width]}),
+        cube({size: [this.width - (2 * 0.064), this.length, this.width - (2 * 0.064)]}).translate([0.064, 0, 0.064])
+    );
+}
+
+
 function makeQuad(mDiameter, mMountWidth, mLength, battLength, battWidth, battHeight, plateThickness) {
-    var bays = makeEngineBays(mMountWidth, mLength);
+    var plateLow = makeLowerPlate(4, 6, plateThickness);
     var booms = makeBooms(mLength, plateThickness);
+    //var bays = makeEngineBays(mMountWidth, mLength);
     var model = union(
+        plateLow,
         booms
     );
     return model;
 }
 
+/*
 function makeEngineBays(mMountWidth, mLength) {
 
 }
+*/
+
+function makeLowerPlate(length, width, thickness) {
+    var plateL = new lowerPlate(length, width, thickness);
+    return plateL.plate.translate([-length / 2, 0, -0.064]);
+}
 
 function makeBooms(mLength, plateThickness) {
-    var arms = union(
-        difference(
-            cube({size: [0.75, mLength, 0.75]}),
-            cube({size: [0.75 - (2 * plateThickness), mLength, 0.75 - (2 * plateThickness)]}).translate([plateThickness, 0, plateThickness])
-        ),
-        color('yellow', difference(
-            cube({size: [(mLength - 0.75) / 2, 0.75, 0.75]}),
-            cube({size: [(mLength - 0.75) / 2, 0.75 - (2 * plateThickness), 0.75 - (2 * plateThickness)]}).translate([0, plateThickness, plateThickness])
-        )).translate([-(mLength - 0.75) / 2, (mLength - 0.75) / 2, 0]),
-        color('yellow', difference(
-            cube({size: [(mLength - 0.75) / 2, 0.75, 0.75]}),
-            cube({size: [(mLength - 0.75) / 2, 0.75 - (2 * plateThickness), 0.75 - (2 * plateThickness)]}).translate([0, plateThickness, plateThickness])
-        )).translate([0.75, (mLength - 0.75) / 2, 0])
-    );
-    echo("**********Boom arms**********");
-    echo("Boom arm 1: " + mLength + " inches");
-    echo("Boom arm 2: " + ((mLength - 0.75) / 2) + " inches");
-    echo("Boom arm 3: " + ((mLength - 0.75) / 2) + " inches");
-    return arms;
+    // define arm lengths
+    var longLength = mLength;
+    var shortLength = (mLength - 0.75) / 2;
+    // generate arm objects
+    var longArm = new boomArm(longLength);
+    var shortArm1 = new boomArm(shortLength);
+    var shortArm2 = new boomArm(shortLength);
+    //...
+    return union(
+        longArm.arm,
+        shortArm1.arm.rotateZ(90).translate([shortLength + longArm.width, shortLength, 0]),
+        shortArm2.arm.rotateZ(90).translate([0, shortLength, 0])
+    ).rotateZ(45).translate([sqrt(pow(shortLength, 2) + pow(shortLength, 2)) / 2, 0, 0]);
 }
