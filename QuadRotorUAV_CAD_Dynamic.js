e60367342d72469bad91b2ba2e73ed51dcf02c73
agnash <agnash@gmail.com>
2013-12-13 12:52:49 -0800

    Update QuadRotorUAV_CAD_Dynamic.js

--- QuadRotorUAV_CAD_Dynamic.js
+++ QuadRotorUAV_CAD_Dynamic.js
@@ -30,6 +30,19 @@ function lowerPlate(length, width, thickness) {
     this.plate = difference(
         cube({size: [this.length, this.width, this.thickness]})
     );
+    
+}
+
+// upper frame plate object constructor
+function upperPlate(length, width, thickness, cutout) {
+    this.length = length;
+    this.width = width;
+    this.thickness = thickness;
+    this.cutout = cutout;
+    this.plate = difference(
+        cube({size: [this.length, this.width, this.thickness]}),
+        cube({size: [this.length - 2*0.5, this.cutout, this.thickness]}).translate([0.5, this.width - this.cutout, 0])
+    );
 }
 
 // boom arm object constructor
@@ -44,12 +57,17 @@ function boomArm(length) {
 
 
 function makeQuad(mDiameter, mMountWidth, mLength, battLength, battWidth, battHeight, plateThickness) {
-    var plateLow = makeLowerPlate(4, 6, plateThickness);
-    var booms = makeBooms(mLength, plateThickness);
+    var plateLength = 4;
+    var plateWidth = 6;
+    var cutout = 1.5;
+    var plateLow = makeLowerPlate(plateLength, plateWidth, plateThickness);
+    var booms = makeBooms(mLength, plateThickness, plateLength);
+    var plateHigh = makeUpperPlate(plateLength, plateWidth, plateThickness, cutout);
     //var bays = makeEngineBays(mMountWidth, mLength);
     var model = union(
         plateLow,
-        booms
+        booms,
+        plateHigh
     );
     return model;
 }
@@ -65,10 +83,11 @@ function makeLowerPlate(length, width, thickness) {
     return plateL.plate.translate([-length / 2, 0, -0.064]);
 }
 
-function makeBooms(mLength, plateThickness) {
+function makeBooms(mLength, plateThickness, plateLength) {
     // define arm lengths
     var longLength = mLength;
     var shortLength = (mLength - 0.75) / 2;
+    var shift = plateLength / 2;
     // generate arm objects
     var longArm = new boomArm(longLength);
     var shortArm1 = new boomArm(shortLength);
@@ -78,5 +97,10 @@ function makeBooms(mLength, plateThickness) {
         longArm.arm,
         shortArm1.arm.rotateZ(90).translate([shortLength + longArm.width, shortLength, 0]),
         shortArm2.arm.rotateZ(90).translate([0, shortLength, 0])
-    ).rotateZ(45).translate([sqrt(pow(shortLength, 2) + pow(shortLength, 2)) / 2, 0, 0]);
+    ).rotateZ(45).translate([sqrt(pow(shortLength, 2) + pow(shortLength, 2)) / 2, -1*(sqrt((pow(mLength / 2, 2)) / 2) - (shift - (sqrt(pow(0.75, 2)/2)/2))), 0]);
+}
+
+function makeUpperPlate(length, width, thickness, cutout) {
+    var plateH = new upperPlate(length, width, thickness, cutout);
+    return plateH.plate.translate([-length / 2, 0, 0.75]);
 }
