1576080715cf6d0af1cbb189a89b1d0a7782328d
agnash <agnash@gmail.com>
2013-12-15 23:13:20 -0800

    Update QuadRotorUAV_CAD_Dynamic.js

--- QuadRotorUAV_CAD_Dynamic.js
+++ QuadRotorUAV_CAD_Dynamic.js
@@ -4,7 +4,7 @@ function getParameterDefinitions() {
   return [
     { name: 'mDiameter', type: 'float', initial: 35.0, caption: "Motor diameter (mm):"},
     { name: 'mMountWidth', type: 'float', initial: 58.0, caption: "Motor mount diameter (mm):"},
-    { name: 'mLength', type: 'float', initial: 20, caption: "Motor-to-motor distance:"},
+    { name: 'mLength', type: 'float', initial: 17.5, caption: "Motor-to-motor distance:"},
     { name: 'battLength', type: 'float', initial: 5.0, caption: "Length of battery:" },
     { name: 'battWidth', type: 'float', initial: 2.0, caption: "Width of battery:" },
     { name: 'battHeight', type: 'float', initial: 1.0, caption: "Height of battery:" },
@@ -77,9 +77,10 @@ function makeQuad(mDiameter, mMountWidth, mLength, battLength, battWidth, battHe
     var plateLength = 4;
     var plateWidth = 6;
     var cutout = 1.5;
-    var refHeight = 5.25;    // temporary battery/payload height reference height
+    var camSpace = 1;
+    var refHeight = 4.5;    // temporary battery/payload height reference height
     var heightReference = color('black', cylinder({r: 0.125, h: refHeight}));   // temporary battery/payload height reference cylinder
-    var battPlate = makeBattPlate(plateWidth, cutout);
+    var battPlate = makeBattPlate(plateWidth, camSpace);
     var plateLow = makeLowerPlate(plateLength, plateWidth, plateThickness);
     var booms = makeBooms(mLength, plateThickness, plateLength);
     var plateHigh = makeUpperPlate(plateLength, plateWidth, plateThickness, cutout);
@@ -108,7 +109,7 @@ function makeBattPlate(width, cutout) {
     var thickness = 0.125;
     var drop = 1.25;
     var plateB = new batteryPlate(length, adjustedWidth, thickness);
-    return plateB.plate.translate([-length / 2, cutout, -drop]);
+    return plateB.plate.translate([-length / 2, cutout, -(drop + 0.064)]);
 }
 
 function makeLowerPlate(length, width, thickness) {
@@ -139,19 +140,23 @@ function makeUpperPlate(length, width, thickness, cutout) {
 }
 
 function makeStandoffs() {
+    var fromEdge = 0.25;
     var frameSpace = 0.75;
     var battSpace = 1.25;
     var frameSpacer = new standoff(frameSpace);
     var battSpacer = new standoff(battSpace);
     return union(
-        frameSpacer.standoff.translate([0, 0.3, 0]),      // frame spacer: bow
-        frameSpacer.standoff.translate([1.7, 2, 0]),      // frame spacer: port
-        frameSpacer.standoff.translate([0, 3.7, 0]),      // frame spacer: stern
-        frameSpacer.standoff.translate([-1.7, 2, 0]),     // frame spacer: starboard
-        frameSpacer.standoff.translate([1.7, 4.8, 0]),    // frame spacer: port quarter fore
-        frameSpacer.standoff.translate([1.7, 5.7, 0]),    // frame spacer: port quarter aft
-        frameSpacer.standoff.translate([-1.7, 4.8, 0]),   // frame spacer: starboard quarter fore
-        frameSpacer.standoff.translate([-1.7, 5.7, 0]),   // frame spacer: starboard quarter aft
-        battSpacer.standoff.translate([7, 0, -(battSpace + 0.064)])       // battery spacer: port bow - TEMP LOCATION
+        frameSpacer.standoff.translate([0, fromEdge, 0]),                                   // frame spacer: bow
+        frameSpacer.standoff.translate([2 - fromEdge, 2, 0]),                               // frame spacer: port
+        frameSpacer.standoff.translate([0, 4 - fromEdge, 0]),                               // frame spacer: stern
+        frameSpacer.standoff.translate([-(2 - fromEdge), 2, 0]),                            // frame spacer: starboard
+        frameSpacer.standoff.translate([2 - fromEdge, 4.5 + fromEdge, 0]),                  // frame spacer: port quarter fore
+        frameSpacer.standoff.translate([2 - fromEdge, 6 - fromEdge, 0]),                    // frame spacer: port quarter aft
+        frameSpacer.standoff.translate([-(2 - fromEdge), 4.8, 0]),                          // frame spacer: starboard quarter fore
+        frameSpacer.standoff.translate([-(2 - fromEdge), 5.7, 0]),                          // frame spacer: starboard quarter aft
+        battSpacer.standoff.translate([1 - fromEdge, 1 + fromEdge, -(battSpace + 0.064)]),  // battery spacer: port bow
+        battSpacer.standoff.translate([-1 + fromEdge, 1 + fromEdge, -(battSpace + 0.064)]), // battery spacer: starboard bow
+        battSpacer.standoff.translate([1 - fromEdge, 3 - fromEdge, -(battSpace + 0.064)]),  // battery spacer: port quarter
+        battSpacer.standoff.translate([-1 + fromEdge, 3 - fromEdge, -(battSpace + 0.064)])  // battery spacer: starboard quarter
     );
 }
